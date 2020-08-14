f56903a57ebe8db3aa2dbff6407e0d259ed5028f
CodingCCarpenter <Christineassists@aol.com>
2020-08-13 22:32:08 -0500

    create scaffolding for NavButon

---
+++ src/components/buttons/NavButton.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NavButton = () => {
+    // state will go here => need href
+};
+
+export default NavButton;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
+// component imports
+import NavButton from '../../components/'
+
 const Header = (props) => {
 
+   return (
+       <div>
+           <ul>
+               <li>
+                    <NavButton />    
+               </li>
+               <li>
+
+               </li>
+           </ul>
+       </div>
+   )
 };
 
 export default Header;
