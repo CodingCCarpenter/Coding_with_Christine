ab6e4748d3b4455afd6a7cf6bd31817d71086d8b
CodingCCarpenter <Christineassists@aol.com>
2020-08-13 22:44:45 -0500

    correct typo in inport of NavButton inside of Header.jsx

--- src/components/buttons/NavButton.jsx
+++ src/components/buttons/NavButton.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const NavButton = () => {
+const NavButton = (props) => {
     // state will go here => need href
+
+    return (
+        <div>
+            <button>{props.linkTo}</button>
+        </div>
+    )
 };
 
 export default NavButton;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
 // component imports
-import NavButton from '../../components/'
+import NavButton from '../buttons/NavButton'
 
+// style imports
+import "./Header.css"
 const Header = (props) => {
 
    return (
        <div>
            <ul>
                <li>
-                    <NavButton />    
+                    <NavButton linkTo='sign up'/>    
                </li>
                <li>
-                    <NavButton />
+                    <NavButton linkTo='log in'/>
+               </li>
+               <li>
+               <li>
+                    <NavButton linkTo='guest'/>
+               </li>
                </li>
            </ul>
        </div>
