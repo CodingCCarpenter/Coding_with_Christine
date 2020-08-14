35627914f49f7039857a47a35a66e803b04bee0b
CodingCCarpenter <Christineassists@aol.com>
2020-08-13 22:14:49 -0500

    adding components folder and skeleton for Header.jsx.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Header from './components/header/Header.jsx'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header />
     </div>
   );
 }
