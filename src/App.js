940fd05a12f6d2a3e181bdc77482b9197a7372a1
CodingCCarpenter <Christineassists@aol.com>
2020-08-13 22:22:54 -0500

    import and render Footer in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
+// Component imports
 import Header from './components/header/Header.jsx'
+import Footer from './components/header/Header.jsx'
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Footer />
     </div>
   );
 }
